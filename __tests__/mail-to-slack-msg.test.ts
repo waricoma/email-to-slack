1e1de7f1f00a6f60a84bce2ad425c96dcd96d49d
waricoma <waricoma@devne.co>
2020-08-21 18:44:09 +0900

    :white_check_mark: add: the test of "whenThereIsDate"

--- __tests__/mail-to-slack-msg.test.ts
+++ __tests__/mail-to-slack-msg.test.ts
@@ -9,3 +9,18 @@ test('if set the email list of "from", "getSenderAndReceiver" will return it.', 
 
   expect(mailToSlackMsg.getSenderAndReceiver()).toBe('> from Taro Yamada tyamada@example.com / hanako@example.com');
 });
+
+test('if set the email list of "to", "getSenderAndReceiver" will return it.', () => {
+  const mailToSlackMsg = new MailToSlackMsg({
+    to: [{ name: 'Taro Yamada', address: 'tyamada@example.com' }, { address: 'hanako@example.com' }],
+  });
+
+  expect(mailToSlackMsg.getSenderAndReceiver()).toBe('> to Taro Yamada tyamada@example.com / hanako@example.com');
+});
+
+test('if set the date, "whenThereIsDate" will return it.', () => {
+  const date = new Date().toString();
+  const mailToSlackMsg = new MailToSlackMsg({ date });
+
+  expect(mailToSlackMsg.whenThereIsDate()).toBe(`:clock2: \`${date}\``);
+});
